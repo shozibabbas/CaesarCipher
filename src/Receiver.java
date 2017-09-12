7c8a6bc07a1d56d8421f2f659152a9b3569f0e78
shozibabbas <14bscssabbas@seecs.edu.pk>
2017-09-12 20:01:10 +0500

    complete working code

---
+++ src/Receiver.java
@@ -0,0 +1,21 @@
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Receiver {
+    public static void main(String[] args) {
+        try {
+            ServerSocket Server = new ServerSocket(5000);
+            Socket socket = Server.accept();
+            DataInputStream dIn = new DataInputStream(socket.getInputStream());
+            System.out.println(CaesarCipher.decrypt(dIn.readUTF()));
+            //dIn.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
