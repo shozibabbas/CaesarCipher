12472486d540ebe3dd0ca025b7ca5e8e7d239bc5
shozibabbas <14bscssabbas@seecs.edu.pk>
2017-09-12 21:00:33 +0500

    changed the fixed length of characters to match the encryption_string length Removed programming bugs (warnings by the compiler)

--- src/CaesarCipher.java
+++ src/CaesarCipher.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class CaesarCipher {
 
-    public static final String ENCRYPTION_STRING = "abcdefghijklmnopqrstuvwxyz";
+    private static final String ENCRYPTION_STRING = "abcdefghijklmnopqrstuvwxyz";
     private static final int SHIFT_KEY = 9;
 
     public static void main(String[] args) {
@@ -10,7 +10,7 @@ public class CaesarCipher {
         Scanner in = new Scanner(System.in);
 
         System.out.print("Enter the String for Encryption: ");
-        String message = new String();
+        String message;
         message = in.next();
 
         String encryptedText = encrypt(message);
@@ -23,31 +23,31 @@ public class CaesarCipher {
     }
 
     public static String encrypt(String plainMessage) {
-        String cipherText = "";
+        StringBuilder cipherText = new StringBuilder();
         for (int i = 0; i < plainMessage.length(); i++)
         {
             int charPosition = ENCRYPTION_STRING.indexOf(plainMessage.charAt(i));
-            int plainChar = (SHIFT_KEY + charPosition) % 26;
+            int plainChar = (SHIFT_KEY + charPosition) % ENCRYPTION_STRING.length();
             char cipherChar = ENCRYPTION_STRING.charAt(plainChar);
-            cipherText += cipherChar;
+            cipherText.append(cipherChar);
         }
-        return cipherText;
+        return cipherText.toString();
     }
     public static String decrypt(String cipherText) {
-        String plainMessage = "";
+        StringBuilder plainMessage = new StringBuilder();
         cipherText = cipherText.toLowerCase();
         for (int i = 0; i < cipherText.length(); i++)
         {
             int charPosition = ENCRYPTION_STRING.indexOf(cipherText.charAt(i));
-            int cipherChar = (charPosition - SHIFT_KEY) % 26;
+            int cipherChar = (charPosition - SHIFT_KEY) % ENCRYPTION_STRING.length();
             if (cipherChar < 0)
             {
                 cipherChar = ENCRYPTION_STRING.length() + cipherChar;
             }
             char plainChar = ENCRYPTION_STRING.charAt(cipherChar);
-            plainMessage += plainChar;
+            plainMessage.append(plainChar);
         }
-        return plainMessage;
+        return plainMessage.toString();
     }
 
 }
